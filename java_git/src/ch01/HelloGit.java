eddaf1ab87774d62f124d3d6d1d8a19d42a7fb06
ksm09 <ksm09@DESKTOP-7506PNR>
2019-07-31 15:07:14 +0900

    2차 커밋~

--- java_git/src/ch01/HelloGit.java
+++ java_git/src/ch01/HelloGit.java
@@ -3,7 +3,7 @@ package ch01;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git~~~~!!!");
+		System.out.println("다시변경 !! Hello Git~~~~!!!");
 	}
 }
 
